b5d6b54f8cff3832156703382cafde552f6bf24e
siddharthasarma <sidduinevitable@gmail.com>
2018-04-14 16:05:18 +0530

    Refactored the code :fire:

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './Navbar';
-import Search from './Search';
-import Card from './Card';
+import Movies from './Movies';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.searchMovies = this.searchMovies.bind(this);
-  }
-  state = {
-    movies: [],
-  };
-
-  searchMovies(movie) {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=8f253d49332ce1bab9054b6c0b810226&language=en-US&query=${movie}`)
-      .then(res => res.json())
-      .then(movies => this.setState({ movies: movies.results }));
-  }
-
-  render() {
-    return (
-      <div className="container-fluid">
-        <Navbar />
-        <Search onSearch={this.searchMovies} />
-        <div className="row">
-          {this.state.movies.map(movie => <Card key={movie.id} movie={movie} />)}
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="container-fluid">
+    <Navbar />
+    <Movies />
+  </div>
+);
 
 export default App;
---
+++ src/Movies.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Search from './Search';
+import Card from './Card';
+
+class Movies extends Component {
+  constructor() {
+    super();
+    this.searchMovies = this.searchMovies.bind(this);
+  }
+  state = {
+    movies: [],
+  };
+
+  searchMovies(movie) {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=8f253d49332ce1bab9054b6c0b810226&language=en-US&query=${movie}`)
+      .then(res => res.json())
+      .then(movies => this.setState({ movies: movies.results }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Search onSearch={this.searchMovies} />
+        <div className="row">
+          {this.state.movies.map(movie => <Card key={movie.id} movie={movie} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movies;
