47417a68bed5f9e994dc09c5ccad25058afff625
siddharthasarma <sidduinevitable@gmail.com>
2018-04-29 16:07:59 +0530

    Not done with Pagination complete implementation :construction:

--- src/Pagination/index.js
+++ src/Pagination/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Work in progress
 const Pagination = ( props ) => {
   const pages = props.pages > 10 ? 10 : props.pages;
   const pageValues = Array.from( Array( pages ) ).map( ( ele, index ) => index );
